61b998cfd890db1e6e58b52d3faf73e43b908aac
scalvo <scalvodelcerro@gmail.com>
2018-08-06 11:30:57 +0200

    Configure local PostGIS database

--- archeology_web/settings.py
+++ archeology_web/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'archeology_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'archeology_web',
+        'USER': 'archeology_web_user',
+        'PASSWORD': 'awu#0001',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
