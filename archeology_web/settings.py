6aa0297b84e9b9baccc7466b152b6354f7dafde0
scalvo <scalvodelcerro@gmail.com>
2018-08-06 11:33:58 +0200

    Create app 'user_accounts'

--- archeology_web/settings.py
+++ archeology_web/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'archeology_sites'
+    'archeology_sites',
+    'user_accounts'
 ]
 
 MIDDLEWARE = [
